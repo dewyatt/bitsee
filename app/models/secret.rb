a092476eb9d439eaae5f85d7e670c90b5f011ab0
Daniel Wyatt <Daniel.Wyatt@gmail.com>
2016-12-19 16:57:25 -0500

    Add model validations and default sorting for Secret

--- app/models/secret.rb
+++ app/models/secret.rb
@@ -1,2 +1,10 @@
 class Secret < ApplicationRecord
+  default_scope -> { order(time: :desc) }
+  validates :tx, presence: true, length: { is: 64 }
+  validates :file_mime, presence: true, unless: -> (secret){not secret.file_path.present?}
+  validates :time, presence: true
+  validates :text, presence: true, unless: -> (secret){secret.file_path.present?}
+  validates :file_path, presence: true, unless: -> (secret){secret.text.present?}
+  validates :file_size, presence: true, unless: -> (secret){not secret.file_path.present?}
 end
+

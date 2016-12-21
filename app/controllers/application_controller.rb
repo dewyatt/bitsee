604b2f51259998e9dc6acfa11ff57c67a5366a42
Daniel Wyatt <Daniel.Wyatt@gmail.com>
2016-12-20 22:01:38 -0500

    Initial layout using bootstrap with paginated secrets view.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def index
+    @secrets = Secret.paginate(page: params[:page], per_page: 20)
   end
 
 end
